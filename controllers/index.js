dd256866650c74696e9ea14e390e4339a01b76dc
jinwenli <jiwenli.yanrongyun.com>
2018-05-17 16:31:55 +0800

    added fs

--- controllers/index.js
+++ controllers/index.js
@@ -1,31 +1,12 @@
-const Koa = require('koa');
-const bodyParser = require('koa-bodyparser');
-const router = require('koa-router')();
-
-const app = new Koa();
-
-app.use(async (ctx, next) => {
-    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
-    await next();
-});
-
-app.use(bodyParser());
-
-
-router.get('/hello/:name', async (ctx, next) => {
-    let name = ctx.params.name;
-    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
-});
-
-router.get('/', async (ctx, next) => {
+let fn_index = async (ctx, next) => {
     ctx.response.body = `<h1>Index</h1>
 <form action="/signin" method="post">
 <p>Name: <input name="name" value="koa"></p>
 <p>Password: <input name="password" type="password"></p>
 <p><input type="submit" value="Submit"></p></form>`;
-});
+};
 
-router.post('/signin', async (ctx, next) => {
+let fn_signin = async (ctx, body) => {
     let name = ctx.request.body.name || '',
         password = ctx.request.body.password || '';
     console.log(`signin with name: ${name}, password: ${password}`);
@@ -34,9 +15,9 @@ router.post('/signin', async (ctx, next) => {
     } else {
         ctx.response.body = `<h1>Login failed</h1><p><a href="/">Try again</a></p>`
     }
-});
+};
 
-app.use(router.routes());
-
-app.listen(3300);
-console.log('app startd at port 3300');
+module.exports = {
+    'GET /': fn_index,
+    'POST /signin': fn_signin
+};
